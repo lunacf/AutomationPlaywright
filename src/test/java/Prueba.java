859a93cdef57c93c8dfbd405ea8d54ed09b70234
Facundo <facundolunac@gmail.com>
2025-08-31 14:36:31 -0300

    Primeros tests agregados

--- src/test/java/Prueba.java
+++ src/test/java/Prueba.java
@@ -1,7 +1,15 @@
 import com.microsoft.playwright.*;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.*;
+
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
+
+// npx playwright codegen https://e-commerceerce.vercel.app/ o cualquier app para levantar la grabacion
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class Prueba {
@@ -10,7 +18,7 @@ public class Prueba {
     List<Browser> browsers = new ArrayList<>();
     List<BrowserContext> contexts = new ArrayList<>();
     List<Page> pages = new ArrayList<>();
-
+    static APIRequestContext apiContext;
     String[] browserNames = {"chromium", "firefox", "webkit"};
 
     @BeforeAll
@@ -36,15 +44,82 @@ public class Prueba {
             BrowserContext context = browser.newContext();
             contexts.add(context);
             Page page = context.newPage();
-            page.navigate("https://www.google.com");
             pages.add(page);
+            apiContext = playwright.request().newContext();
+        }
+    }
+
+ /*   @Test
+    @Order(1)
+    @DisplayName("Abrir carrito")
+    void abrirCarrito() {
+        for (Page page : pages) {
+            page.navigate("https://e-commerceerce.vercel.app/");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Shop Now!")).click();
+            assertThat(page.locator(".w-full.gap-10.flex.flex-wrap.items-center.justify-center.my-20")).isVisible();
         }
     }
 
     @Test
-    void openGoogleInAllBrowsers() {
+    @Order(2)
+    @DisplayName("Bajar Documento")
+    void bajarDocumento() {
         for (Page page : pages) {
-            assertThat(page).hasTitle("Google");
+            page.navigate("https://adazzle.github.io/react-data-grid/?utm_source=chatgpt.com#/CommonFeatures");
+            Download download = page.waitForDownload(() -> {
+                page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Export to CSV")).click();
+            });
+            download.saveAs(Paths.get("C:\\Users\\Lu\\Downloads", download.suggestedFilename()));
+        }
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Encontrar primer elemento")
+    void encontrarPrimerElemento() {
+        for (Page page : pages) {
+            page.navigate("https://adazzle.github.io/react-data-grid/?utm_source=chatgpt.com#/CommonFeatures");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Header Filters")).click();
+            assertThat(page.locator(".rdg-row.r1upfr80.rdg-row-even .rdg-cell.cj343x0").nth(2)).isVisible();
+            assertThat(page.locator(".rdg-row.r1upfr80.rdg-row-even .rdg-cell.cj343x0").nth(2)).containsText("Critical");
+        }
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Escribir y esperar evento")
+    void escribirYEsperarEvento() {
+        for (Page page : pages) {
+            page.navigate("http://www.uitestingplayground.com/");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Text Input")).click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Set New Button Name")).click();
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Set New Button Name")).fill("hola");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Button That Should Change it'")).click();
+            assertThat(page.locator("#updatingButton")).containsText("hola");
+            assertThat(page.getByLabel("Subscribe to newsletter")).isChecked();
+
+
+        }
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("API status")
+    void apiStatusTest() {
+        for (Page page : pages) {
+            APIResponse response = apiContext.get("https://demoqa.com/bad-request");
+            assertEquals(200, response.status(), "El GET no devolvió 200 OK");
+        }
+    } */
+
+    @Test
+    @Order(6)
+    @DisplayName("Entrar a url")
+    void entrarUrl() {
+        for (Page page : pages) {
+            page.navigate("https://demoqa.com/");
+            page.locator("div").filter(new Locator.FilterOptions().setHasText(Pattern.compile("^Forms$"))).nth(2).click();
+            assertThat(page).hasURL("https://demoqa.com/forms");
         }
     }
 
